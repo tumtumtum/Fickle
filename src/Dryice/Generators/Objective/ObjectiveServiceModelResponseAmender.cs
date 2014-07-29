166cdbf39fde01dc9d82b70207dd9bab04cb51e9
JeffGos <gosling23@gmail.com>
2014-07-29 10:42:46 +0100

    - renamed the Response amend

---
+++ src/Dryice/Generators/Objective/ObjectiveServiceModelResponseAmender.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using Dryice.Model;
+using Platform;
+
+namespace Dryice.Generators.Objective
+{
+	public class ObjectiveServiceModelResponseAmmender
+		: ServiceModelResponseAmmender
+	{
+		public ObjectiveServiceModelResponseAmmender(ServiceModel serviceModel, CodeGenerationOptions options)
+			: base(serviceModel, options)
+		{
+		}
+
+		protected override ServiceClass CreateValueResponseServiceClass(Type type)
+		{
+			var unwrappedType = type.GetUnwrappedNullableType();
+			
+			string typeName;
+
+			if (unwrappedType.IsNumericType() || unwrappedType == typeof(bool) || (unwrappedType.IsEnum && type.IsNullable()))
+			{
+				typeName = "int?";
+			}
+			else
+			{
+				typeName = TypeSystem.GetPrimitiveName(type);
+			}
+
+			var properties = new List<ServiceProperty>
+			{
+				new ServiceProperty()
+				{
+					Name = options.ResponseStatusPropertyName,
+					TypeName = options.ResponseStatusTypeName
+				}
+			};
+
+			if (type != typeof(void))
+			{
+				properties.Add
+				(
+					new ServiceProperty()
+					{
+						Name = "Value",
+						TypeName = typeName
+					}
+				);
+			}
+
+			return new ServiceClass(ObjectiveBinderHelpers.GetValueResponseWrapperTypeName(type), null, properties);
+		}
+	}
+}
