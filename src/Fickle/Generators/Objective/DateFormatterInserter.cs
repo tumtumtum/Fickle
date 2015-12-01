e45b5921dba2213d7857bd2ef8afc1d78b593853
Thong Nguyen <tumtumtum@gmail.com>
2015-12-01 19:26:52 +0000

    Default to javascript datetime format

--- src/Fickle/Generators/Objective/DateFormatterInserter.cs
+++ src/Fickle/Generators/Objective/DateFormatterInserter.cs
@@ -56,8 +56,9 @@ namespace Fickle.Generators.Objective
 				expressions.Add(Expression.Assign(dateFormatter, Expression.New(new FickleType("NSDateFormatter"))).ToStatement());
 				// [dateFormatter setTimeZone: [NSTimeZone timeZoneWithAbbreviation:@"UTC"]];
 				expressions.Add(FickleExpression.Call(dateFormatter, "setTimeZone", FickleExpression.StaticCall("NSTimeZone", "NSTimeZone", "timeZoneWithAbbreviation", "UTC")).ToStatement());
-				// [dateFormatter setDateFormat: @"yyyy-MM-dd'T'HH:mm:ssZZZZZ"];
-				expressions.Add(FickleExpression.Call(dateFormatter, "setDateFormat", "yyyy-MM-dd'T'HH:mm:ssZZZZZ").ToStatement());
+				// Javascript: yyyy-MM-dd'T'HH:mm:ss.SSSSz
+				// ISO 8601: yyyy-MM-dd'T'HH:mm:ssZZZZZ
+				expressions.Add(FickleExpression.Call(dateFormatter, "setDateFormat", "yyyy-MM-dd'T'HH:mm:ss.SSSSz").ToStatement());
 				
 				expressions.AddRange(block.Expressions);
 
