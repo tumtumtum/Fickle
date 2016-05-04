be443bb614dab64793b8fb3018d94f209d04eb65
Sam Cook <github@sam.net.nz>
2016-05-04 16:40:22 +0100

    Add documentation to FickleIncludeTypeAttribute

--- src/Fickle.WebApi/FickleIncludeTypeAttribute.cs
+++ src/Fickle.WebApi/FickleIncludeTypeAttribute.cs
@@ -6,7 +6,14 @@ namespace Fickle.WebApi
 	public class FickleIncludeTypeAttribute
 		: Attribute
 	{
+		/// <summary>
+		/// The type to include in the Fickle service model
+		/// </summary>
 		public Type Type { get; set; }
+
+		/// <summary>
+		/// Whether to include other types from the assembly that are in the same namespace as the target type
+		/// </summary>
 		public bool IncludeRelatives { get; set; }
 		
 		public FickleIncludeTypeAttribute(Type type)
